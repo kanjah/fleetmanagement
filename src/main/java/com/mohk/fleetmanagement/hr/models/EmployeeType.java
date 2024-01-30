ea82486bfe86e31723c80afbc07f383df377c17e
kanjah <patokanjah@gmail.com>
2024-01-30 16:32:28 +0300

    added _layout.html, routes, controllers, hr-model, settings-model

---
+++ src/main/java/com/mohk/fleetmanagement/hr/models/EmployeeType.java
@@ -0,0 +1,19 @@
+package com.mohk.fleetmanagement.hr.models;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import com.mohk.fleetmanagement.settings.models.CommonObject;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper=false)
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class EmployeeType extends CommonObject {
+
+}
