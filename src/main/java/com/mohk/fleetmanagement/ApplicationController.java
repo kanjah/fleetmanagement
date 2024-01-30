ef911c74bbe0b823d695120d3f910fdf1a9024ec
kanjah <patokanjah@gmail.com>
2024-01-30 16:24:00 +0300

    added _layout.html, routes, controllers, hr-model, settings-model

--- src/main/java/com/mohk/fleetmanagement/ApplicationController.java
+++ src/main/java/com/mohk/fleetmanagement/ApplicationController.java
@@ -1,10 +1,47 @@
 package com.mohk.fleetmanagement;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class ApplicationController {
     @GetMapping("/index")
     public String home(){
         return "index";
+    }
+    @GetMapping("/hr")
+    public String hr(){
+        return "hr/index";
+    }
+
+    @GetMapping("/accounts")
+    public String accounts(){
+        return "accounts/index";
+    }
+    @GetMapping("/reports")
+    public String reports(){
+        return "reports/index";
+    }
+    @GetMapping("/payroll")
+    public String payroll(){
+        return "payroll/index";
+    }
+
+    @GetMapping("/fleet")
+    public String fleet(){
+        return "fleet/index";
+    }
+    @GetMapping("/settings")
+    public String settings(){
+        return "settings/index";
+    }
+
+    @GetMapping("/helpdesk")
+    public String helpdesk(){
+        return "helpdesk/index";
+    }
+
+    @GetMapping("/security")
+    public String security(){
+        return "security/index";
     }
 }
